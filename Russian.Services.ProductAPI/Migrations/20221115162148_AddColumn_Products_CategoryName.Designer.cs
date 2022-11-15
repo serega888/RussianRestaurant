fa3d40181b5bbe48668612c5cf93dde59be12724
Sergey_Fadin <sergey.fadin@outlook.com>
2022-11-15 19:22:53 +0300

    создал миграцию для добавления столбца CategoryName в таблицу Products

--- Russian.Services.ProductAPI/Migrations/20221115162148_AddColumn_Products_CategoryName.Designer.cs
+++ Russian.Services.ProductAPI/Migrations/20221115162148_AddColumn_Products_CategoryName.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Russian.Services.ProductAPI.DbContexts;
 
 namespace Russian.Services.ProductAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221115162148_AddColumn_Products_CategoryName")]
+    partial class AddColumn_Products_CategoryName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
