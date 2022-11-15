4e2704f1ffa0a8a538cf843986b94b1653c199fd
Sergey_Fadin <sergey.fadin@outlook.com>
2022-11-15 18:19:45 +0300

    Создал класс ApplicationDbContext

---
+++ Russian.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Russian.Services.ProductAPI.DbContexts
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+	}
+}
