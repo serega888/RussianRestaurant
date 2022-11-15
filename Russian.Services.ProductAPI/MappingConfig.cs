a60f19106249720e4c19c3cd659ea3375a69d885
Sergey_Fadin <sergey.fadin@outlook.com>
2022-11-16 01:47:49 +0300

    создал класс МаppingConfig

---
+++ Russian.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Russian.Services.ProductAPI.Models;
+using Russian.Services.ProductAPI.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Russian.Services.ProductAPI
+{
+	public class MappingConfig
+	{
+		public static MapperConfiguration RegisterMaps()
+		{
+			var mapperConfig = new MapperConfiguration(config =>
+			{
+				config.CreateMap<Product, ProductDto>().ReverseMap();
+			});
+
+			return mapperConfig;
+		}
+	}
+}
