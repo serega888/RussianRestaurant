a0fc2612675ab55b6549abf1589c21352d88e6a1
Sergey_Fadin <sergey.fadin@outlook.com>
2022-11-16 10:34:35 +0300

    создал статический класс StaticVariable

---
+++ Russian.Web/StaticVariable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Russian.Web
+{
+	public static class StaticVariable
+	{
+		public static string PRODUCTAPIBASE { get; set; }
+		public enum ApiType
+		{
+			GET,
+			POST,
+			PUT,
+			DELETE
+		}
+	}
+}
