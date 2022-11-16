1ece0b2522851b2dbfba60526306e81064187045
Sergey_Fadin <sergey.fadin@outlook.com>
2022-11-16 10:39:13 +0300

    создал запрос АПИ к микросервису

---
+++ Russian.Web/Models/ApiRequestToMicroservice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Russian.Web.StaticVariable;
+
+namespace Russian.Web.Models
+{
+	public class ApiRequestToMicroservice
+	{
+		public ApiType ApiType { get; set; } = ApiType.GET;
+		public string Url { get; set; }
+		public object Data { get; set; }
+		public string AccessToken { get; set; }
+	}
+}
